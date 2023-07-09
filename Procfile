72ca1c5e5e136b81a80942b58159d0ab5cedeb4a
Dariusz Cichoń <silentman89@gmail.com>
2023-07-09 18:49:08 +0200

    update

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: heroku-php-nginx public
 queue: php artisan queue:work --tries=1 --no-ansi
 cron: chmod a+x ./cron.sh && ./cron.sh
-release: php artisan migrate --force
--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -3,10 +3,12 @@ set -eo pipefail;
 
 
 # Create storage paths if missing in persistent storage.
-mkdir -p storage/app/public
-mkdir -p storage/framework/{cache,sessions,testing,views}
-mkdir -p storage/logs
+#mkdir -p storage/app/public
+#mkdir -p storage/framework/{cache,sessions,testing,views}
+#mkdir -p storage/logs
 
 # Link already created in pre-deploy.
 # Run for a confirmation message that it was created successfully.
-php artisan storage:link
+#php artisan storage:link
+
+php artisan migrate --force
