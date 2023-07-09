112e1343272a35176dfb6e98c56108a9b5315178
Dariusz Cichoń <silentman89@gmail.com>
2023-07-09 21:34:19 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Running postdeploy.sh"
+
+sudo apt update
+
+sudo apt install -y nodejs
+
+sudo apt install -y npm
+
+npm install
+
+npm run build
+
 heroku-php-apache2 public/
